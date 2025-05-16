7df62245f3f4fb6a530c9ac1bd573a01d3c0f7cc
davidguarinv <davidguarinv@gmail.com>
2025-05-15 23:42:54 -0400

    Made sure the tailwind config build worked. NOw the styling loads.

--- static/css/config/tailwind.config.js
+++ static/css/config/tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {darkMode: ["class"],
+module.exports = {
+  darkMode: ["class"],
   content: [
-    "./pages/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-    "./app/**/*.{ts,tsx}",
-    "./src/**/*.{ts,tsx}",
-    "*.{js,ts,jsx,tsx,mdx}",
+    "./templates/**/*.{html,py}",
+    "./static/**/*.{html,js,jsx,ts,tsx}",
   ],
   prefix: "",
   theme: {

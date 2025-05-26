bc670e0d9b5379095e3a47a56947106c810ef089
davidguarinv <davidguarinv@gmail.com>
2025-05-26 17:23:04 +0200

    communtiies progress.

---
+++ config.js
@@ -0,0 +1,6 @@
+// OpenCage Geocoding API configuration
+const config = {
+    OPENCAGE_API_KEY: '48bcf64904a14f418e6920d63a9945a8' // You'll need to replace this with your actual API key
+};
+
+export default config;
